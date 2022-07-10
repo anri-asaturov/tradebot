00b87e322a75dc8658b7a4fc1af0ccde7481093a
Anri Asaturov <anri82@gmail.com>
2022-07-10 22:40:33 +0300

    initial

---
+++ build/index.test.js
@@ -0,0 +1,8 @@
+import { run } from './index.js';
+import { expect, describe, it } from 'vitest';
+describe('', () => {
+    it('test', () => {
+        // import run function and test it
+        expect(run()).toBe('i run');
+    });
+});
---
+++ src/index.test.ts
@@ -0,0 +1,9 @@
+import { run } from './index.js';
+import { expect, describe, it } from 'vitest';
+
+describe('', () => {
+  it('test', () => {
+    // import run function and test it
+    expect(run()).toBe('i run');
+  });
+});
